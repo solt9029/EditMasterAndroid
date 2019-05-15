71d3a18d2f5261add18990fc34fd6e8d6fb02375
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-05-15 16:43:35 +0900

    constants files added

---
+++ app/src/main/java/com/solt9029/editmasterandroid/constant/PercentageConstants.java
@@ -0,0 +1,11 @@
+package com.solt9029.editmasterandroid.constant;
+
+public class PercentageConstants {
+    public class Player {
+        public static final int SPEED_TO_SPACE_WIDTH = 3;
+    }
+
+    public class Editor {
+        public static final double BAR_START_LINE = 0.02;
+    }
+}
