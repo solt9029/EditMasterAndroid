22f525aaa47a45824ad9c357dd2cb55454b14dbb
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-05-17 16:15:56 +0900

    di kotlin

---
+++ app/src/main/java/com/solt9029/editmasterandroid/di/ViewModelKey.kt
@@ -0,0 +1,10 @@
+package com.solt9029.editmasterandroid.di
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
