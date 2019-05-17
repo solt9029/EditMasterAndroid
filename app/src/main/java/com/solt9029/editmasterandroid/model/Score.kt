300931a0d4f3221ee96db04a9f3d861b7e55b96f
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-05-17 16:43:41 +0900

    kotlin

---
+++ app/src/main/java/com/solt9029/editmasterandroid/model/Score.kt
@@ -0,0 +1,16 @@
+package com.solt9029.editmasterandroid.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Score(
+        val id: Int?,
+        val username: String?,
+        val comment: String?,
+        @field:SerializedName("video_id") val videoId: String?,
+        val bpm: Float?,
+        val offset: Float?,
+        val speed: Float?,
+        val notes: List<Int>?,
+        @field:SerializedName("advanced_settings") val advancedSettings: Any?,
+        @field:SerializedName("created_at") val createdAt: String?,
+        @field:SerializedName("updated_at") val updatedAt: String?)
