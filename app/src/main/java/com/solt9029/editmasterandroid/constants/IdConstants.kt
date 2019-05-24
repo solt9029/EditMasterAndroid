461123ad87318a43bc25b0f285286460d58b8ec9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-05-24 12:35:21 +0900

    currentNote and currentDivision

--- app/src/main/java/com/solt9029/editmasterandroid/constants/IdConstants.kt
+++ app/src/main/java/com/solt9029/editmasterandroid/constants/IdConstants.kt
@@ -1,8 +1,18 @@
 package com.solt9029.editmasterandroid.constants
 
 class IdConstants {
-    enum class Note {
-        SPACE, DON, KA, BIGDON, BIGKA, RENDA, BIGRENDA, BALLOON
+    // note ids correspond to the radio button group item positions
+    object Note {
+        const val DON = 0
+        const val KA = 1
+        const val BIGDON = 2
+        const val BIGKA = 3
+        const val RENDA = 4
+        const val BIGRENDA = 5
+        const val BALLOON = 6
+        const val SPACE = 7
+        const val COPY = 8
+        const val PASTE = 9
     }
 
     enum class State {
