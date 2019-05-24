b16ff28fde3b2470d59ee6efaf0d98e8175e0755
Kenshi Shiode <kshiode@yahoo-corp.jp>
2019-05-24 18:15:52 +0900

    add calc

--- app/src/main/java/com/solt9029/editmasterandroid/view/customview/EditorNotesView.kt
+++ app/src/main/java/com/solt9029/editmasterandroid/view/customview/EditorNotesView.kt
@@ -7,10 +7,7 @@ import android.graphics.PixelFormat
 import android.graphics.PorterDuff
 import android.util.AttributeSet
 import android.view.SurfaceHolder
-import com.solt9029.editmasterandroid.constants.IdConstants
-import com.solt9029.editmasterandroid.constants.NumberConstants
-import com.solt9029.editmasterandroid.constants.PercentageConstants
-import com.solt9029.editmasterandroid.constants.PositionConstants
+import com.solt9029.editmasterandroid.constants.*
 import com.solt9029.editmasterandroid.util.CalcUtil
 import com.solt9029.editmasterandroid.util.IndexRange
 
@@ -19,6 +16,7 @@ class EditorNotesView : BaseSurfaceView, SurfaceHolder.Callback {
     private var notes: List<Int>? = null
 
     private val editorBarXPx = CalcUtil.convertDp2Px(PositionConstants.EDITOR_BAR_X, context)
+    private val editorBarOutsideHeightPx = CalcUtil.convertDp2Px(SizeConstants.EDITOR_BAR_OUTSIDE_HEIGHT, context)
 
     constructor(context: Context) : super(context)
 
@@ -50,15 +48,23 @@ class EditorNotesView : BaseSurfaceView, SurfaceHolder.Callback {
 
         val barWidthPx = CalcUtil.calcBarWidthPx(width, context)
         val actualBarWidthPx = barWidthPx * (1 - PercentageConstants.EDITOR_BAR_START_LINE)
-        val spaceWidth = actualBarWidthPx / NumberConstants.NOTES_PER_BAR
+        val spaceWidthPx = actualBarWidthPx / NumberConstants.NOTES_PER_BAR
         val barStartLineXPx = editorBarXPx + barWidthPx * PercentageConstants.EDITOR_BAR_START_LINE
         val range: IndexRange = CalcUtil.calcNoteIndexRangeInEditor(notes!!.size, translateYPx, height, context)
 
         for (i in range.last downTo range.first) {
-            val note: Int = notes?.get(i) ?: continue
+            val note: Int = notes!![i]
             if (note == IdConstants.Note.SPACE) {
                 continue
             }
+            val c: Int = i % NumberConstants.NOTES_PER_BAR
+            val l: Int = Math.floor(i.toDouble() / NumberConstants.NOTES_PER_BAR).toInt()
+            val x = barStartLineXPx + spaceWidthPx * c
+            val y = editorBarOutsideHeightPx * (l + 0.5)
+
+            val previousNote = if (i > 0) notes!![i - 1] else IdConstants.Note.SPACE
+            val nextNote = if (i < notes!!.size - 1) notes!![i + 1] else IdConstants.Note.SPACE
+
             // draw notes here
         }
 
