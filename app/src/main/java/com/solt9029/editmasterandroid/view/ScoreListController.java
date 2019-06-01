a3e5fbeac53745ff9be64cf1def758a284983dfe
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-06-01 22:52:42 +0900

    error

--- app/src/main/java/com/solt9029/editmasterandroid/view/ScoreListController.java
+++ app/src/main/java/com/solt9029/editmasterandroid/view/ScoreListController.java
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.airbnb.epoxy.AutoModel;
 import com.airbnb.epoxy.Typed2EpoxyController;
+import com.solt9029.editmasterandroid.ErrorItemBindingModel_;
 import com.solt9029.editmasterandroid.ProgressBarItemBindingModel_;
 import com.solt9029.editmasterandroid.ScoreItemBindingModel_;
 import com.solt9029.editmasterandroid.model.Score;
+import com.solt9029.editmasterandroid.util.SafeUnboxUtil;
 import com.solt9029.editmasterandroid.viewmodel.Resource;
 
 import java.util.List;
@@ -14,6 +16,9 @@ import java.util.List;
 public class ScoreListController extends Typed2EpoxyController<Resource<List<Score>>, Boolean> {
     @AutoModel
     ProgressBarItemBindingModel_ progressBarItemBindingModel;
+    @AutoModel
+    ErrorItemBindingModel_ errorItemBindingModel;
+
     private OnItemClickListener listener;
 
     public ScoreListController(OnItemClickListener listener) {
@@ -29,12 +34,13 @@ public class ScoreListController extends Typed2EpoxyController<Resource<List<Sco
 
         if (resource.getData() != null) {
             for (Score score : resource.getData()) {
-                new ScoreItemBindingModel_().score(score).listener(listener).id(score.getId()).addTo(this);
+                int id = SafeUnboxUtil.safeUnbox(score.getId());
+                new ScoreItemBindingModel_().score(score).listener(listener).id(id).addTo(this);
             }
         }
 
         progressBarItemBindingModel.addIf(resource.isLoading() && !isRefreshing, this);
-
+        errorItemBindingModel.addIf(resource.getError() != null, this);
     }
 
     public interface OnItemClickListener {
